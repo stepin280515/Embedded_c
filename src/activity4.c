bd1910b1e4f6bc01ed519f8983637861fb7ecc22
stepin280515 <82246243+stepin280515@users.noreply.github.com>
2021-04-30 17:51:49 +0530

    Update activity4.c

--- src/activity4.c
+++ src/activity4.c
@@ -8,7 +8,7 @@
 
 
 
-void USARTInit(uint16_t ubrr_value){
+void USART_Init(uint16_t ubrr_value){
     USART_BAUD_PIN_L = ubrr_value;
     USART_BAUD_PIN_H  =SET_BAUD_H;
     USART_CONTROL_PIN_C =SET_CONTROL_PIN_C_8BIT;
@@ -21,7 +21,7 @@ void USARTInit(uint16_t ubrr_value){
  * @param data 
  */
 
-void USARTWriteChar(int data){
+void USART_WriteChar(int data){
     if(data==1){
         while(TRANSMITTING_DATA){}
         USART_DATA_PIN='2';
