655c3eeaa23ed5edafc0316a17749c47ff581e1a
stepin280515 <82246243+stepin280515@users.noreply.github.com>
2021-04-30 17:35:45 +0530

    Update and rename SeatHeatingApp.c to main.c

--- main.c
+++ main.c
@@ -28,8 +28,8 @@ int main(void)
                 temp = Read_ADC_Value(0);
                 d=outputfromPWM(temp);
                 _delay_ms(20);
-                USART_Init(103);
-            USART_WriteChar(d);
+                USARTInit(103);
+            USARTWriteChar(d);
             
                 
             }
