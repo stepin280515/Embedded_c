1039599db718fe090d7119c07a31b59232b76100
stepin280515 <82246243+stepin280515@users.noreply.github.com>
2021-04-30 17:20:00 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -2,27 +2,14 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "activity3.h"
-
-
-/**
- * @brief A function to setup timer1 with channel A for pulse width modulation with wave generation mode of 10 bits fast PWM and prescaling of 64.
- * 
- */
-void TimerWaveGenMode(void){
+void TimerWaveGenerationMode(void){
 
     SET_TIMER1A_FAST_PWM; //setting timer1 for PWM
     PRESCALE_TIMER_64; //8 prescalar
     SET_PWM_AS_OUTPUT; //Setting PB1 as output pin
 
 }
-
-/**
- * @brief A function which receives 10 bit of ADC input from temperature sensor and outputs as a fast PWM waveform.
- * 
- * @param ADC_val 
- * @return char
- */
-int outputbyPWM(uint16_t ADC_val)
+int outputfromPWM(uint16_t ADC_val)
 {if(ADC_val<=209){
 
         OCR1A = 205; //20% duty cycle
