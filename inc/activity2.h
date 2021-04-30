7a6c25396062866cc96d4d433906d75e3b9f1791
stepin280515 <82246243+stepin280515@users.noreply.github.com>
2021-04-30 17:11:29 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -5,7 +5,7 @@
  * @brief A function to initialise the analog to digital converter
  * 
  */
-void Init_ADC();
+void ADC_Init();
 
 /**
  * @brief A function to receive temperature sensor inputs from analog pin and output it to a register ADC which has 2 8 bit registers ADCL and ADCH.
@@ -13,7 +13,7 @@ void Init_ADC();
  * @param ch 
  * @return uint16_t 
  */
-uint16_t Read_ADC(uint8_t ch);
+uint16_t Read_ADC_Value(uint8_t ch);
 
 
 
